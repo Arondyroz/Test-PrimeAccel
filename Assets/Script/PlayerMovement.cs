851ed4e751d640266d6f02c0ae351823704667fb
Andhika <andhikarriyadi@gmail.com>
2022-02-08 15:49:22 +0700

    New File

---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PrimeAcceleratorClone
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+        [SerializeField] private float moveSpeed;
+
+        private Animator anim;
+        private Rigidbody rb;
+        // Start is called before the first frame update
+        void Start()
+        {
+            anim = GetComponent<Animator>();
+            rb = GetComponent<Rigidbody>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            MovePlayer();
+        }
+
+        private void FixedUpdate()
+        {
+            
+        }
+
+        void MovePlayer()
+        {
+            if(Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                anim.SetBool("B_Run", true);
+            }
+
+        }
+    }
+}
