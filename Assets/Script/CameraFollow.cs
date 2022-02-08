b3216506bd171abfc6871690e6095d6d37e4e7cb
Andhika <andhikarriyadi@gmail.com>
2022-02-08 21:50:25 +0700

    New Update

---
+++ Assets/Script/CameraFollow.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace PrimeAcceleratorClone
+{
+    public class CameraFollow : MonoBehaviour
+    {
+        public Transform targetPlayer;
+
+        [SerializeField] private float smoothSpeed;
+        [SerializeField] private Vector3 offset;
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            CameraFollowPlayer();
+        }
+
+        void CameraFollowPlayer()
+        {
+            Vector3 cameraPos = targetPlayer.position + offset;
+            Vector3 smoothedPos = Vector3.Lerp(transform.position, cameraPos, smoothSpeed * Time.deltaTime);
+            transform.position = smoothedPos;
+        }
+    }
+}
--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -6,35 +6,91 @@ namespace PrimeAcceleratorClone
 {
     public class PlayerMovement : MonoBehaviour
     {
-        [SerializeField] private float moveSpeed;
+        public static PlayerMovement instance;
+        public PlayerInput playerInput;
+        public enum PlayerInput
+        {
+            Idle,
+            LeftMove,
+            RightMove,
+        }
 
+        [SerializeField] private float moveSpeed;
         private Animator anim;
         private Rigidbody rb;
         // Start is called before the first frame update
+        private void Awake()
+        {
+            if (instance != null)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                instance = this;
+            }
+        }
         void Start()
         {
             anim = GetComponent<Animator>();
             rb = GetComponent<Rigidbody>();
+            playerInput = PlayerInput.Idle;
         }
 
         // Update is called once per frame
         void Update()
         {
+            UpdateMovePlayer();
+        }
+
+        void UpdateMovePlayer()
+        {
             MovePlayer();
-        }
-
-        private void FixedUpdate()
-        {
-            
-        }
-
-        void MovePlayer()
-        {
-            if(Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.LeftArrow))
+            if (Input.GetKey(KeyCode.LeftArrow))
             {
-                anim.SetBool("B_Run", true);
+                playerInput = PlayerInput.LeftMove;
+            }
+
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                playerInput = PlayerInput.RightMove;
+            }
+
+            if(Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.RightArrow))
+            {
+                playerInput = PlayerInput.Idle;
             }
 
         }
+        void MovePlayer()
+        {
+            switch(playerInput)
+            {
+                case PlayerInput.Idle:
+                    anim.SetBool("B_Run", false);
+                    break;
+                case PlayerInput.LeftMove:
+                    anim.SetBool("B_Run", true);
+                    rb.velocity = new Vector3(-moveSpeed * Time.deltaTime, 0f, 0f);
+                    //transform.Translate(new Vector3(1 * moveSpeed * Time.deltaTime, 0, 0));
+                    if (transform.rotation.y != 270f)
+                    {
+                        transform.rotation = Quaternion.Euler(0, 270f, 0);
+                    }
+                    break;
+                case PlayerInput.RightMove:
+                    anim.SetBool("B_Run", true);
+                    rb.velocity = new Vector3(moveSpeed * Time.deltaTime, 0f, 0f);
+                    //transform.Translate(new Vector3(1 * -moveSpeed * Time.deltaTime, 0, 0));
+                    if (transform.rotation.y != 90f)
+                    {
+                        transform.rotation = Quaternion.Euler(0, 90f, 0);
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+
     }
 }
