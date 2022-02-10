be314a39b5e02288460ab4d3cd68332f65a525ff
Andhika <andhikarriyadi@gmail.com>
2022-02-10 16:49:57 +0700

    Final Update

---
+++ Assets/Script/UIManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PrimeAcceleratorClone
+{
+    public class UIManager : MonoBehaviour
+    {
+        public void QuitGame()
+        {
+            Application.Quit();
+        }
+    }
+}
